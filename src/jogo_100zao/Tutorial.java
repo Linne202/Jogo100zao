fad13bd15685661eca522de72e4083f2a30f1e19
unknown <lannalenymelo@gmail.com>
2023-03-04 15:43:26 -0300

    ajustado tutorial

--- src/jogo_100zao/Tutorial.java
+++ src/jogo_100zao/Tutorial.java
@@ -3,7 +3,18 @@ package jogo_100zao;
 public class Tutorial {
 	
 	public static void mostrar() {
-		System.out.println("AQUI TEM UM TUTORIAL");
+		
+		System.out.println("................ T U T O R I A L .................\n");
+		System.out.println("O jogo terá 10 perguntas aleatórias.");
+		System.out.println("A cada pergunta seram 4 alternativa de A até D,");
+	    System.out.println("sendo uma correta.");
+        System.out.println("Caso você acerte a primeira pergunta, ganhará um prêmio");
+		System.out.println("entre R$0,50 e R$3,00, se você acertar as próximas");
+		System.out.println("perguntas o seu prêmio será dobrado.");
+		System.out.println("No decorrer do jogo você terá a opção de desistir");
+		System.out.println("se desistir terá um desconto de 10% a 35%");
+		System.out.println("do valor do seu prêmio.");	
+	    System.out.println("Se você errar alguma questão, você perde o ");
+	    System.out.println("jogo e o prêmio.\n");
 	}
-
 }
