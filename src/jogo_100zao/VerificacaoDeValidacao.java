a6567c6b7ca6c720f6859e16af51a98cc66ab3c8
Linne202 <decassialine6@gmail.com>
2023-03-19 18:33:20 -0300

    Alteração na classe VerificacaoDeValidacao

--- src/jogo_100zao/VerificacaoDeValidacao.java
+++ src/jogo_100zao/VerificacaoDeValidacao.java
@@ -10,57 +10,30 @@ import java.util.Scanner;
  */
 
 public class VerificacaoDeValidacao {
-	
 
-	public static boolean textoDesistencia() {
+	@SuppressWarnings("resource")
+
+	public static boolean TextoDesejaDesistir(double premioAcumulado) {
+
+		System.out.println("Você deseja desistir?");
+		System.out.println("Digite 1 para SIM e 2 para NÃO");
 
 		Scanner sc = new Scanner(System.in);
-		int resposta;
-		System.out.println("Você deseja desistir?");
-		System.out.println("Digite - 1 para SIM ou - 2 para NÃO");
-		LeitorDeDados.leitorDeNumeroInteiro();
+		int resposta = sc.nextInt();
 
-		do {
+		if (resposta == 1) {
+			double valorFinal = TaxaDesistencia.calcularPremioFinal(premioAcumulado);
+			System.out.println("Seu saldo: R$" + String.format("%1$,.2f\n", valorFinal));
+			System.out.println("... I S S O   É   T U D O   P E S S O A L ...\n");
 
-			Menu.mostrarMenu();
-			resposta = sc.nextInt();
+			sc.close();
 
-			switch (resposta) {
-			case 1:
-				resposta = sc.nextInt();
-				System.out.println("Obrigado, até mais!!!");
-				break;
-			case 2:
-				return false;
-			default:
-				resposta = sc.nextInt();
-				System.out.println("Valor digitado invalido\n");
-			}
-			
-		} while (resposta != 0);
+			return true;
 
-		sc.close();
+		}
+		
 		return false;
+
 	}
 
-		public static boolean desejaDesistir(double premioAcumulado) {
-
-			System.out.println("Você deseja desistir?");
-			System.out.println("Digite 1 para SIM e 2 para NÃO");
-
-			Scanner sc = new Scanner(System.in);
-			int resposta = sc.nextInt();
-
-			if (resposta == 1) {
-				double valorFinal = TaxaDesistencia.calcularPremioFinal(premioAcumulado);
-				System.out.println("Seu saldo: R$" + String.format("%1$,.2f\n", valorFinal));
-				System.out.println("... I S S O   É   T U D O   P E S S O A L ...\n");
-				return true;
-			}
-			return false;
-			
-		}
-	
 }
-
-
